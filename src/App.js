f1a38fd7dd45fda78a67096a297b5d0431c14882
Brittany Blake <brittany.blake49@gmail.com>
2020-12-08 17:44:31 -0500

    add nav and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {
+  Switch, Route, Redirect, NavLink,
+} from 'react-router-dom';
 import Forms from './components/forms';
 import DistanceHome from './containers/DistanceHome';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <NavLink to="/">
+        <Nav />
+      </NavLink>
       <Switch>
         <Route path="/" exact component={Forms} />
         <Route path="/auto" exact component={DistanceHome} />
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+import useStyles from './Nav.styles';
+
+export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = useStyles();
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link to="/">Calculate distance Manually</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to="/auto"> Calculate distance with coordinates</Link>
+            </MenuItem>
+          </Menu>
+          <Typography variant="h6" className={classes.title}>
+            Hedyla Assessment
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
