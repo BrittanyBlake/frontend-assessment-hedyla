e1df40cd39b7fddce15fc7686daeef2dbeea9f5f
Brittany Blake <brittany.blake49@gmail.com>
2020-12-09 15:27:37 -0500

    add polyline

--- src/components/maps/MapContainer.js
+++ src/components/maps/MapContainer.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-// Marker,
+import {
+  Map, Marker, Polyline, GoogleApiWrapper,
+} from 'google-maps-react';
+
+const triangleCoords = [
+  { lat: 17.294005687070193, lng: -62.686008717148326 },
+  { lat: 17.285236780568827, lng: -62.69656589128129 },
+];
 
 const MapContainer = ({ google }) => (
   <div>
     <Map
-      style={{ width: '600px', height: '600px' }}
+      style={{
+        height: '75vh',
+      }}
       google={google}
       initialCenter={{
         lat: 17.294005687070193,
         lng: -62.686008717148326,
       }}
-    />
+    >
+      <Marker
+        position={{ lat: 17.294005687070193, lng: -62.686008717148326 }}
+      />
+      <Marker position={{ lat: 17.285236780568827, lng: -62.69656589128129 }} />
+      <Polyline
+        path={triangleCoords}
+        strokeColor="#0000FF"
+        strokeOpacity={0.8}
+        strokeWeight={2}
+      />
+    </Map>
   </div>
 );
-
 // export default MapContainer
 
 export default GoogleApiWrapper({
