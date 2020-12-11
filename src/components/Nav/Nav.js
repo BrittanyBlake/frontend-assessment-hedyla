f9a0223906848c5c884c1256c74fa31ad92cdde2
Brittany Blake <brittany.blake49@gmail.com>
2020-12-10 21:48:10 -0500

    restyle nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import Link from "@material-ui/core/Link";
 import { Link } from 'react-router-dom';
-// import Container from '@material-ui/core/Container';
-// import { Typography } from '@material-ui/core';
-// import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 import Logo from '../../assets/hedyla.png';
+import Logo2 from '../../assets/logo.png';
 import useStyles from './Nav.styles';
 
 const ButtonAppBar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [show, handleShow] = useState(false);
   const classes = useStyles();
 
   const handleClick = event => {
@@ -26,60 +23,104 @@ const ButtonAppBar = () => {
     setAnchorEl(null);
   };
 
+  useEffect(() => {
+    /* eslint-disable no-unused-expressions */
+    window.addEventListener('scroll', () => {
+      window.scrollY > 95 ? handleShow(true) : handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" color="transparent" elevation={0}>
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: show === true ? '#FFFFFF' : '#13151D',
+        }}
+        elevation={0}
+      >
         <Toolbar>
-
-          <Link to="/">
-            <img
-              src={Logo}
-              alt="logo"
-              style={{ height: '45px' }}
-            />
-          </Link>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            Forms
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>
-              <Link to="/">Calculate distance Manually</Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Link to="/auto"> Calculate distance with coordinates</Link>
-            </MenuItem>
-          </Menu>
-          <Button
-            href="https://github.com/BrittanyBlake"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </Button>
-          <Button
-            href="https://brittany-blake.netlify.app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Portfolio
-          </Button>
-          <Button
-            href="https://www.linkedin.com/in/brittany-a-blake/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </Button>
+          <Grid container spacing={3}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{
+                textAlign: 'left',
+                paddingLeft: '45px',
+              }}
+            >
+              <Link to="/">
+                <img
+                  src={Logo}
+                  alt="logo"
+                  style={{
+                    height: '45px',
+                    display: show === false ? 'block' : 'none',
+                  }}
+                />
+                <img
+                  src={Logo2}
+                  alt="logo"
+                  style={{
+                    height: '45px',
+                    display: show === true ? 'block' : 'none',
+                  }}
+                />
+              </Link>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Button
+                style={{ color: show === false ? 'white' : '#276690' }}
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                Forms
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Link to="/">Calculate distance Manually</Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Link to="/auto"> Calculate distance with coordinates</Link>
+                </MenuItem>
+              </Menu>
+              <Button
+                style={{ color: show === false ? 'white' : '#276690' }}
+                href="https://github.com/BrittanyBlake"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </Button>
+              <Button
+                style={{ color: show === false ? 'white' : '#276690' }}
+                href="https://brittany-blake.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Portfolio
+              </Button>
+              <Button
+                style={{ color: show === false ? 'white' : '#276690' }}
+                href="https://www.linkedin.com/in/brittany-a-blake/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
